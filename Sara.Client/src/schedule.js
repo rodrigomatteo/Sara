fc2616002f934b0820a7a456cbfd798fa5f52bed
Rodrigo Matteo <rodrigo.matteo@gmail.com>
2018-06-24 17:35:45 -0500

    Added calendar plugin (fullcalendar)

--- Sara.Client/src/schedule.js
+++ Sara.Client/src/schedule.js
@@ -1,3 +1,22 @@
-export class Schedule {
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
 
+@inject(HttpClient)
+export class Schedule {
+  events = [];
+
+  constructor(http) {
+    http.configure(c => {
+      c.useStandardConfiguration()
+        .withBaseUrl('http://localhost:3000/');
+    });
+
+    this.http = http;
+  }
+
+  activate() {
+    return this.http.fetch('events')
+      .then(response => response.json())
+      .then(events => this.events = events);
+  }
 }
