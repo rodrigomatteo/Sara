c99defe86f3ce0adcf379e6d51380465ab0fd1a0
Rodrigo Matteo <rodrigo.matteo@gmail.com>
2018-07-12 22:17:03 -0500

    Changes in routing and new filter position

--- Sara.Client/src/app.js
+++ Sara.Client/src/app.js
@@ -5,8 +5,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Sara';
     config.map([
-      { route: '', redirect: 'content' },
-      { route: 'content', name: 'content', moduleId: 'content', nav: true, title: 'Root' },
+      { route: '', name: 'root', redirect: 'schedule' },
+      { route: 'content', name: 'content', moduleId: 'content', nav: true, title: 'Dashboard' },
       { route: 'schedule', name: 'schedule', moduleId: 'schedule', nav: true, title: 'Schedule' },
       { route: 'match', name: 'match', moduleId: 'match', nav: false, title: 'Match' },
       { route: 'matchday', name: 'matchday', moduleId: 'matchday', nav: false, title: 'Match Day' }
